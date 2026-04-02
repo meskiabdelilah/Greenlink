f77cae27e1a38c4d01ec0b45aa4039926898e4c5
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-04-02 22:46:41 +0100

    feat: implement model of point transaction

---
+++ app/Models/PointTransaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PointTransaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'deposit_id',
+        'description',
+        'points',
+        'type'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function deposit()
+    {
+        return $this->belongsTo(Deposit::class);
+    }
+}
