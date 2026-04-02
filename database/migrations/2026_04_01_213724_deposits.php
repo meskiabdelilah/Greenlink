3c081cb8bde069c20e9d88bc186089cf47395d5f
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-04-02 09:37:47 +0100

    feat: implement migration of deposits

---
+++ database/migrations/2026_04_01_213724_deposits.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('citizen_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained('waste_categories')
+                ->cascadeOnDelete();
+
+            $table->decimal('estimated_weight', 10, 2)
+                ->nullable();
+            $table->decimal('actual_weight', 10, 2)
+                ->nullable();
+            $table->string('photo_path')
+                ->nullable();
+            $table->enum('status', ['pending', 'assigned', 'collected','validated']);
+
+
+            $table->timestamp('collected_at')
+                ->nullable();
+            $table->timestamp('validated_at')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deposits');
+    }
+};
