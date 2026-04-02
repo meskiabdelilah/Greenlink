458eb608a619208822711dd7ec8a1302a2a178c6
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-04-02 22:46:05 +0100

    feat: implement model of deposit

---
+++ app/Models/Deposit.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Deposit extends Model
+{
+    protected $fillable = [
+        'citizen_id',
+        'agent_id',
+        'category_id',
+        'estimated_weight',
+        'actual_weight',
+        'photo_path',
+        'collected_at',
+        'validated_at',
+        'status'
+    ];
+
+    public function citizen()
+    {
+        return $this->belongsTo(User::class, 'citizen_id');
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(User::class, 'agent_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(WasteCategory::class);
+    }
+
+    public function pointTransaction()
+    {
+        return $this->hasOne(PointTransaction::class);
+    }
+}
